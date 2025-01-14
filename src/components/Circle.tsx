c3af6656d6c49786340cc17538633083555f6bf8
ThamizhiniyanCS <csthamizhiniyan@gmail.com>
2025-01-14 23:19:47 +0530

    Completed Hero Section Animation

--- src/components/Circle.tsx
+++ src/components/Circle.tsx
@@ -1,14 +1,27 @@
 import type { ComponentPropsWithoutRef } from "react";
 import { twMerge } from "tailwind-merge";
+import { motion } from "framer-motion";
 
-const Circle = ({ className, children }: ComponentPropsWithoutRef<"div">) => {
+const Circle = ({
+  className,
+  children,
+  animate = false,
+}: ComponentPropsWithoutRef<"div"> & { animate?: boolean }) => {
   return (
     <div
       className={twMerge(
-        "bg-zinc-900 size-[240px] inline-flex justify-center items-center rounded-full outline outline-[6px] -outline-offset-[6px] outline-fuchsia-500/10",
+        "relative bg-zinc-900 size-[240px] inline-flex justify-center items-center rounded-full",
         className
       )}
     >
+      <motion.div
+        animate={animate && { rotate: 360 }}
+        transition={{ ease: "linear", duration: 15, repeat: Infinity }}
+        className={twMerge(
+          "absolute inset-0 rounded-full outline outline-[6px] -outline-offset-[6px] outline-fuchsia-500/10 border-[6px] border-transparent",
+          animate && "border-t-fuchsia-500/30"
+        )}
+      />
       {children}
     </div>
   );
